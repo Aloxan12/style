c155ba5b744830d32c1d505605006d0880d3a1d4
Aloxan12 <kvakvaaak@mail.ru>
2023-05-25 16:32:52 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,12 @@ function App() {
             <div className="col-1">col</div>
             <div className="col-1">col</div>
         </div>
+        <div className='row'>
+            <div className="col-2">col</div>
+            <div className="col-3">col</div>
+            <div className="col-4">col</div>
+            <div className="col-3">col</div>
+        </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at autem consectetur consequatur, delectus dicta doloribus eligendi eos esse et explicabo fugit id illum ipsum itaque iure laborum libero magnam nemo neque odit officiis omnis pariatur placeat possimus, provident quaerat quam quas quia quod recusandae repellendus sequi sit temporibus unde veritatis voluptates. Accusantium aperiam aut, cumque deserunt ducimus eum nihil possimus rem repudiandae sit? Accusantium alias architecto, assumenda autem consequatur cumque, dicta, eligendi enim eum ex fugit id magni maiores nemo nobis officiis pariatur quia quisquam quod rem sit sunt tempore veniam? Esse incidunt, temporibus! Corporis iste reiciendis veritatis voluptatum?</p>
     </div>
   );
