f6d8d782fe39e201956812a7d977095285df86a4
Aloxan12 <kvakvaaak@mail.ru>
2023-05-25 16:20:16 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,20 @@ function App() {
                 <div className="block">col</div>
             </div>
         </div>
+        <div className='row'>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+            <div className="col-1">col</div>
+        </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at autem consectetur consequatur, delectus dicta doloribus eligendi eos esse et explicabo fugit id illum ipsum itaque iure laborum libero magnam nemo neque odit officiis omnis pariatur placeat possimus, provident quaerat quam quas quia quod recusandae repellendus sequi sit temporibus unde veritatis voluptates. Accusantium aperiam aut, cumque deserunt ducimus eum nihil possimus rem repudiandae sit? Accusantium alias architecto, assumenda autem consequatur cumque, dicta, eligendi enim eum ex fugit id magni maiores nemo nobis officiis pariatur quia quisquam quod rem sit sunt tempore veniam? Esse incidunt, temporibus! Corporis iste reiciendis veritatis voluptatum?</p>
     </div>
   );
