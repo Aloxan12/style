8faf1309f1e4ad8507b8ffe3c312c0e470ea62e2
Aloxan12 <kvakvaaak@mail.ru>
2023-05-26 12:11:21 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import {Header} from "./components/AppHeader/Header";
 import {AppPreview} from "./components/AppPreview/AppPreview";
 import {AppList} from "./components/AppList/AppList";
+import {AppEmployees} from "./components/AppEmployees/AppEmployees";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <AppPreview />
         <AppList />
+        <AppEmployees />
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at autem consectetur consequatur, delectus dicta doloribus eligendi eos esse et explicabo fugit id illum ipsum itaque iure laborum libero magnam nemo neque odit officiis omnis pariatur placeat possimus, provident quaerat quam quas quia quod recusandae repellendus sequi sit temporibus unde veritatis voluptates. Accusantium aperiam aut, cumque deserunt ducimus eum nihil possimus rem repudiandae sit? Accusantium alias architecto, assumenda autem consequatur cumque, dicta, eligendi enim eum ex fugit id magni maiores nemo nobis officiis pariatur quia quisquam quod rem sit sunt tempore veniam? Esse incidunt, temporibus! Corporis iste reiciendis veritatis voluptatum?</p>
     </div>
   );
---
+++ src/components/AppEmployees/AppEmployees.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AppEmployees.scss'
+export const AppEmployees = () => {
+    return (
+        <div className='employee'>
+            <div className="employee__item">employee</div>
+            <div className="employee__item">employee</div>
+            <div className="employee__item">employee</div>
+        </div>
+    );
+}
