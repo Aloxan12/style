d765279828f2d39c1a1a01031eac8d71764ef734
Aloxan12 <kvakvaaak@mail.ru>
2023-05-26 13:19:18 +0300

    effects

--- src/components/AppEffectsList/AppEffectsList.tsx
+++ src/components/AppEffectsList/AppEffectsList.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './AppEffectsList.scss'
 
+const text = 'текст должен исчесзуть при наведении текст должен исчесзуть при наведении текст должен исчесзуть при наведении текст должен исчесзуть при наведении текст должен исчесзуть при наведении текст должен исчесзуть при наведениитекст должен исчесзуть при наведении текст должен исчесзуть при наведении'
+
 export const AppEffectsList = () => {
+    const [disappearingIndex, setDisappearingIndex] = useState<{ [key: number]: boolean }>({})
+
+    const onMouseHandler = (index: number) => {
+        if (!disappearingIndex[index]) {
+            setDisappearingIndex(prevState => ({...prevState, [index]: true}))
+        }
+    }
     return (
         <div className='effects-list'>
-            <div className="effects-list__item">item</div>
+            <div className="effects-list__item disappearing-text">
+                {text.split('').map((letter, index) => {
+                    return <span
+                        key={`let${index}`}
+                        className={`${disappearingIndex[index] ? 'active' : ''}`}
+                        onMouseEnter={() => onMouseHandler(index)}
+                        onMouseLeave={() => onMouseHandler(index)}
+                    >{letter}</span>
+                })}
+            </div>
             <div className="effects-list__item">item</div>
             <div className="effects-list__item">item</div>
         </div>
